e84438aeb907b72f026e951af59edc282c51679f
河田鷹介 <benefium@gmail.com>
2023-09-05 14:00:55 +0900

    md5にエンディアンサポートを追加

--- srcs/md5.c
+++ srcs/md5.c
@@ -1,5 +1,7 @@
 #include "ft_ssl_md5_internal.h"
 
+extern int g_is_little_endian;
+
 static void	print_states(unsigned int abcd[4]) {
 	printf("%08x %08x %08x %08x\n", abcd[0], abcd[1], abcd[2], abcd[3]);
 }
@@ -36,6 +38,7 @@ static t_md5_digest	derive_digest(const t_md5_state* state) {
 	md5_word_t	ABCD[] = { state->A, state->B, state->C, state->D };
 	size_t		digest_index = 0;
 	for (size_t i = 0; i < sizeof(ABCD) / sizeof(md5_word_t); ++i) {
+		ABCD[i] = PASS_LIT_END(ABCD[i]);
 		for (size_t j = 0; j < sizeof(md5_word_t); ++j) {
 			digest.digest[digest_index] = ABCD[i] % 256;
 			ABCD[i] /= 256;
