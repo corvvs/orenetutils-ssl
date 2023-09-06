89a73021d66bb4fd9ebf6205365b98cb1dc754ef
河田鷹介 <benefium@gmail.com>
2023-09-06 23:15:53 +0900

    SHA-512

---
+++ srcs/sha_512.c
@@ -0,0 +1,44 @@
+#include "ft_ssl_sha_2_internal.h"
+#include "ft_ssl_common.h"
+#include "ft_ssl_structure.h"
+#include "ft_ssl.h"
+
+extern int g_is_little_endian;
+
+static t_sha_512_state	init_state(const uint8_t *message, uint64_t message_len) {
+	t_sha_512_state state = SHA_512_INITIAL_STATE(message, message_len);
+	ft_memcpy(state.H, SHA_512_H0, sizeof(SHA_512_H0));
+	return state;
+}
+
+static bool	block_operation(t_sha_512_state* state) {
+	DEBUGWARN("BLOCK OPERATION for [" U64T ", " U64T ")", state->block_from, state->block_from + SHA_512_WORD_BLOCK_BIT_SIZE);
+
+	// パディング
+	sha_512_block_padding(state);
+
+	// 演算
+	sha_512_block_rounds(state);
+
+	// ブロックバッファをクリア
+	ft_memset(state->schedule.W, 0, sizeof(state->schedule.W));
+	DEBUGWARN("END BLOCK OPERATION for [" U64T ", " U64T ")", state->block_from, state->block_from + SHA_512_WORD_BLOCK_BIT_SIZE);
+	state->block_from += SHA_512_WORD_BLOCK_BIT_SIZE;
+	return state->block_from < state->padded_message_len;
+}
+
+// ハッシュフロー
+declare_hash_flow(sha_512, SHA_512)
+
+static void print_digest(const t_sha_512_digest* digest) {
+	for (size_t i = 0; i < sizeof(digest->digest) / sizeof(uint8_t); i++) {
+		printf("%02x", digest->digest[i]);
+	}
+}
+
+declare_print_digest_line(sha_512, "SHA2-512")
+
+void	digest_sha_512(const t_preference* pref, const t_message* message) {
+	t_sha_512_digest digest = sha_512_hash(message->message, message->message_bit_len);
+	print_digest_line(pref, message, &digest);
+}
---
+++ srcs/sha_512_derive_digest.c
@@ -0,0 +1,18 @@
+#include "ft_ssl_sha_2_internal.h"
+
+extern int g_is_little_endian;
+
+t_sha_512_digest	sha_512_derive_digest(const t_sha_512_state* state) {
+	t_sha_512_digest	digest = {};
+	sha_512_word_t		H[8];
+	ft_memcpy(H, state->H, sizeof(H));
+	size_t		digest_index = 0;
+	for (size_t i = 0; i < SHA_512_DIGEST_BIT_SIZE / (sizeof(sha_512_word_t) * OCTET_BIT_SIZE); ++i) {
+		H[i] = PASS_BIG_END(H[i]);
+		for (size_t j = 0; j < sizeof(sha_512_word_t); ++j, ++digest_index) {
+			digest.digest[digest_index] = H[i] & ((1u << OCTET_BIT_SIZE) - 1);
+			H[i] >>= OCTET_BIT_SIZE;
+		}
+	}
+	return digest;
+}
