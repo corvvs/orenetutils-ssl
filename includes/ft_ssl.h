6b290fe06554f76efc74862a3c0855f0520ce01b
河田鷹介 <benefium@gmail.com>
2023-09-07 19:06:39 +0900

    コマンド単位の独立性を高める構造変更

--- includes/ft_ssl.h
+++ includes/ft_ssl.h
@@ -6,55 +6,19 @@
 #include "ft_ssl_structure.h"
 #include "ft_ssl_lib.h"
 
-typedef enum e_command {
-	COMMAND_MD5,
-	COMMAND_SHA224,
-	COMMAND_SHA256,
-	COMMAND_SHA384,
-	COMMAND_SHA512,
-	COMMAND_SHA512_224,
-	COMMAND_SHA512_256,
-	COMMAND_UNKNOWN
-}	t_command;
-
-typedef struct e_command_pair {
-	const char* name;
-	t_command	algo;
-}	t_command_pair;
-
-#define DEF_COMMAND_PAIR(n, a) (t_command_pair){ .name = n, .algo = a }
-
-static const t_command_pair g_command_pairs[] = {
-	DEF_COMMAND_PAIR("md5", COMMAND_MD5),
-	DEF_COMMAND_PAIR("sha224", COMMAND_SHA224),
-	DEF_COMMAND_PAIR("sha256", COMMAND_SHA256),
-	DEF_COMMAND_PAIR("sha384", COMMAND_SHA384),
-	DEF_COMMAND_PAIR("sha512", COMMAND_SHA512),
-	DEF_COMMAND_PAIR("sha512-224", COMMAND_SHA512_224),
-	DEF_COMMAND_PAIR("sha512-256", COMMAND_SHA512_256),
-};
-
-typedef struct s_master {
-	const char*		program_name;
-	t_command		command;
-	const char*		command_name;
-	t_preference	pref;
-}	t_master;
-
-
 // md5.c
-void digest_md5(const t_preference* pref, const t_message* message);
+void digest_md5(const t_preference_digest* pref, const t_message* message);
 
 // sha_***.c
-void digest_sha_224(const t_preference* pref, const t_message* message);
-void digest_sha_256(const t_preference* pref, const t_message* message);
-void digest_sha_384(const t_preference* pref, const t_message* message);
-void digest_sha_512(const t_preference* pref, const t_message* message);
-void digest_sha_512_224(const t_preference* pref, const t_message* message);
-void digest_sha_512_256(const t_preference* pref, const t_message* message);
+void digest_sha_224(const t_preference_digest* pref, const t_message* message);
+void digest_sha_256(const t_preference_digest* pref, const t_message* message);
+void digest_sha_384(const t_preference_digest* pref, const t_message* message);
+void digest_sha_512(const t_preference_digest* pref, const t_message* message);
+void digest_sha_512_224(const t_preference_digest* pref, const t_message* message);
+void digest_sha_512_256(const t_preference_digest* pref, const t_message* message);
 
 // preference.c
-int	parse_options(const t_master* master, char** argv, t_preference* pref_ptr);
+int	parse_options_digest(const t_master* master, char** argv, t_preference_digest* pref_ptr);
 
 // read_file.c
 bool	read_file(const t_master* master, int ifd, t_elastic_buffer* buffer_ptr);
--- srcs/sha_512_256.c
+++ srcs/sha_512_256.c
@@ -34,7 +34,7 @@ define_print_digest(sha_512_256)
 
 define_print_digest_line(sha_512_256, "SHA2-512/256")
 
-void	digest_sha_512_256(const t_preference* pref, const t_message* message) {
+void	digest_sha_512_256(const t_preference_digest* pref, const t_message* message) {
 	t_sha_512_256_digest digest = sha_512_256_hash(message->message, message->message_bit_len);
 	print_digest_line(pref, message, &digest);
 }
