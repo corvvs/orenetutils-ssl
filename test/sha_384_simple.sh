082476a085a3595fcd88d0de733d300d281280ec
河田鷹介 <benefium@gmail.com>
2023-09-10 16:49:08 +0900

    環境のopensslでテストする

--- test/sha_384_simple.sh
+++ test/sha_384_simple.sh
@@ -1,4 +1,4 @@
 arg=${1}
 echo -n "${arg}" | ./ft_ssl sha384 -q 2> /dev/null > mine
-echo -n "${arg}" | openssl/apps/openssl sha384  2> /dev/null | ruby -e 'puts gets.split[-1]' > ref
+echo -n "${arg}" | openssl sha384  2> /dev/null | ruby -e 'puts gets.split[-1]' > ref
 diff mine ref
